52473c4519c3b3cf678963d1a2e29adcd58484ba
sde1rihankhan <sde1rihankhan@gmail.com>
2025-07-22 16:43:37 +0530

    HTML changes

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-let nextPage = document.querySelector(".contant8")
+let nextPag = document.querySelector(".contant8")
 let inputs = document.querySelectorAll(".contant2")
 
-
-nextPage.addEventListener("click",()=>{
+nextPag.addEventListener("click",()=>{
     let value = []
+    let isEmpty = false
 
     inputs.forEach(input =>{
         value.push(input.value)
+        if(input.value.trim() === ""){
+            isEmpty = true
+            input.style.border = "2px solid red"
+        }
     })
-    localStorage.setItem("userdata", JSON.stringify(value))
-    console.log(value);
-    
-})
+    inputs.forEach(input =>{
+        input.addEventListener("click",()=>{
+            input.style = "none"
+        })
+    })
 
-function newPage(){
-    window.location.href = "http://127.0.0.1:5502/Musicart/home%20page/index.html"
-}
+    if(isEmpty){
+        alert("Please fill all inputs!")
+        return
+    }
+    localStorage.setItem("userdata", JSON.stringify(value))
+
+    window.location.href = "http://127.0.0.1:5502/Musicart/homePage/index.html"
+    
+    inputs.forEach(input =>{
+        input.value = ""
+    })
+})
